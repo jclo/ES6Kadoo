b4214440675d0c90f3367e912d3dd3a132d4c417
jclo <jclo@mobilabs.fr>
2024-02-10 18:10:30 +0100

    Released 2.1.2.

--- _dist/lib/es6kadoo.mjs
+++ _dist/lib/es6kadoo.mjs
@@ -1,5 +1,5 @@
 /*! ****************************************************************************
- * ES6Kadoo v2.1.1
+ * ES6Kadoo v2.1.2
  *
  * A template for writing pure ES6 Javascript libraries.
  * (you can download it from npm or github repositories)
@@ -126,14 +126,14 @@ const $__ES6GLOB = {};
       const obj = Object.create(methods);
       obj._library = {
         name: 'ES6Kadoo',
-        version: '2.1.1',
+        version: '2.1.2',
       };
       return obj;
     };
 
     // Attaches constants to ES6Kadoo that provide name and version of the lib.
     ES6Kadoo.NAME = 'ES6Kadoo';
-    ES6Kadoo.VERSION = '2.1.1';
+    ES6Kadoo.VERSION = '2.1.2';
 
 
     // -- Private Static Methods -----------------------------------------------
